8976f87e7b1a5937ede9bb3faa6417b380b6aac9
Leeiss <farrakhova.2004@mail.ru>
2023-12-15 02:16:42 +0300

    done

--- main.cpp
+++ main.cpp
@@ -3,14 +3,13 @@
 #include <vector>
 #include <thread>
 #include <chrono>
-#include <mutex>
+#include <mutex> // для предотвращения одновременного потока к общим данным 
 #include <iomanip>
 #include <limits>
 
 std::mutex result_mutex;
 double result = 0.0;
 
-// Функция, вычисляющая интеграл методом прямоугольников на заданном интервале
 double integrate(double start, double end, int steps) {
     double step_size = (end - start) / steps;
     double sum = 0.0;
@@ -18,7 +17,6 @@ double integrate(double start, double end, int steps) {
     for (int i = 0; i < steps; ++i) {
         double x = start + (i + 0.5) * step_size;
 
-        // Добавим проверку на близость x к 0
         if (std::abs(x) > 1e-10) {
             sum += (std::cos(x) - 1) / x;
         }
@@ -33,8 +31,7 @@ void integrateThread(double start, double end, int steps) {
 
     // Защищаем доступ к общей переменной result с использованием мьютекса
     std::lock_guard<std::mutex> lock(result_mutex);
-    
-    // Проверяем, содержит ли результат NaN
+
     if (!std::isnan(partial_result)) {
         result += partial_result;
     }
@@ -43,50 +40,36 @@ void integrateThread(double start, double end, int steps) {
 int main() {
     double x;
     int N;
-    
-    // Ввод данных
-    std::wcout << L"Граница: ";
-    std::wcin >> x;
-    std::wcout << L"Шаги: ";
-    std::wcin >> N;
 
-    // Время начала выполнения программы
+    std::cout << "Граница: ";
+    std::cin >> x;
+    std::cout << "Шаги: ";
+    std::cin >> N;
+
     auto start_time = std::chrono::steady_clock::now();
 
-    // Количество потоков
-    const int num_threads[] = {1, 2, 4, 8, 16, 32};
+    const int num_threads[] = { 1, 2, 4, 8, 16, 32 };
     std::vector<std::thread> threads;
 
-    // Подсчет интеграла на разном количестве потоков
     for (int num_thread : num_threads) {
         result = 0.0;
 
-        // Создаем потоки
         for (int i = 0; i < num_thread; ++i) {
             double thread_start = i * x / num_thread;
             double thread_end = (i + 1) * x / num_thread;
             threads.emplace_back(integrateThread, thread_start, thread_end, N / num_thread);
         }
 
-        // Ожидаем завершения всех потоков
-        for (auto &thread : threads) {
+        for (auto& thread : threads) {
             thread.join();
         }
 
-        // Вывод результата и времени выполнения
         auto end_time = std::chrono::steady_clock::now();
         std::chrono::duration<double> elapsed_time = end_time - start_time;
 
-        // Проверяем, содержит ли результат NaN
-        if (!std::isnan(result)) {
-            std::wcout << L"Результат с " << num_thread << L" потоками: " << std::setprecision(10) << result << std::endl;
-        } else {
-            std::wcout << L"Результат с " << num_thread << L" потоками: Расчет содержит NaN." << std::endl;
-        }
-        
-        std::wcout << L"Время выполнения: " << elapsed_time.count() << L" секунд" << std::endl;
+        std::cout << "Результат с " << num_thread << " потоками: " << std::setprecision(10) << result << std::endl;
+        std::cout << "Время выполнения: " << elapsed_time.count() << " секунд" << std::endl;
 
-        // Очищаем вектор потоков перед следующей итерацией
         threads.clear();
     }
 
